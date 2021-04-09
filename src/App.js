009c30ae917871ef385901b5651f38b57a997f23
jeremyljr <jeremyjiaren@gmil.com>
2021-04-09 16:42:26 +0100

    three.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './containers/homePage/Navbar';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HeroSection from './containers/homePage/HeroSection';
 
+// import { softShadow } from "drei"
+
+// softShadow(); 
+
 function App() {
   return (
     <>
--- src/containers/homePage/HeroSection.js
+++ src/containers/homePage/HeroSection.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useRef, useState, Suspense } from 'react'
 import { Button } from '../../components/Buttons'
 import './HeroSection.css';
 
+import { Canvas, useFrame, useLoader } from "react-three-fiber"; 
+import { useSpring, a} from 'react-spring/three'
+
+
+const Box = ( {position, args}) => {
+    const mesh = useRef(null)
+    useFrame(() => (mesh.current.rotation.y = mesh.current.rotation.z += 0.01))
+
+    const [expand, setExpand] = useState(false);
+
+    const props = useSpring({
+        scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1]
+    })
+
+    return(
+        <a.mesh onClick={() => setExpand(!expand)} scale={props.scale} castShadow position={position} ref={mesh}>
+            <cylinderBufferGeometry attach='geometry' args={args} />
+            <meshStandardMaterial attach='material' color='gold' />
+            
+        </a.mesh>
+    )
+};
+
 function HeroSection() {
+
     return (
         <div className='heroContainer'>
             <div className="heroLeft">
@@ -18,7 +42,35 @@ function HeroSection() {
                     </Button>
                 </div>
             </div>
-            
+            <Canvas shadowMap colorManagement camera={{position: [-5, 2, 10], fov: 50}}>
+                <ambientLight intensity={0.3} />
+                <directionalLight 
+                    castShadow 
+                    position={[0, 10, 0]} 
+                    intensity={1} 
+                    shadow-mapSize-width={1024} 
+                    shadow-mapSize-height={1024} 
+                    shadow-camera-far={50}
+                    shadow-camera-left={10}
+                    shadow-camera-right={10}
+                    shadow-camera-top={10}
+                    shadow-camera-bottom={-10}
+                    />
+                <pointLight position={[-10, 0, -20]} instensity={0.2} />
+                <pointLight position={[0, -10, 0]} instensity={1.5} />
+
+                <group>
+                    <mesh receiveShadow rotation={ [-Math.PI / 2,0,0]} position={[0, -3, 0]}>
+                        <planeBufferGeometry attach='geometry' args={[100, 100]} />
+                        {/* this casts a shadow */}
+                        <meshStandardMaterial attach='material' color='gold' />
+                        <shadowMaterial attach='material' opacity={1}/>
+                    </mesh>
+                        <Box position={[0, 2, 0]} args={[1, 1, 0.2, 32]} />
+                        <Box position={[-2, 1, -5]} args={[1, 1, 0.2, 32]} />
+                        <Box position={[5, 0, -2]} args={[1, 1, 0.2, 32]} />                    
+                </group>
+            </Canvas>
         </div>
     )
 }
