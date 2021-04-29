481a687012f350551192d60d9723ac30c5889173
jeremyljr <jeremyjiaren@gmil.com>
2021-04-29 18:53:01 +0100

    icons

--- src/containers/homePage/MiddleHero.js
+++ src/containers/homePage/MiddleHero.js
@@ -17,28 +17,28 @@ function MiddleHero() {
                 <div className='allFeatures'>
                     <div className='featureLists'>
                         <div className='features'>
-                            <img className='featureLogo' src='https://spshurl.herokuapp.com/C0Aa4' alt='picture'/>
+                            <img className='featureLogo' src='https://s3-alpha-sig.figma.com/img/e2a9/bc36/3c62980934a25ddafe34e076d6a98f1b?Expires=1620604800&Signature=KpNjtYkBasCKnm5nPN9oKCafs0sxeJyMqIIRU~-htVL5SIfzBiysPyu5Rir~fGvjh63NRNDCWEQ3G1CffsMzXlNsUQK7vqaBuPuxEtX~FrSdiHoCJ~M0c4-Phru8i2DahRksf3vj0MN~Hxc6LLTT7U9AJE1~BTBgy6HiVCBdiZdySeb84OZobJtQ~vKubi0lZT-6WsY--hYObpYPrxOgKrY5~u~QFwjKCrmZb2SKgQ4iaox~1CX5Yz7xRTd8c4mFVzNPN5aNdt5z8HdbVd66HkelL~iDiSs6bL0hu0jWQcP2OwMUByPaXpXbknFoaTRuJiOzbaHLxvUxqkDSZOjhZQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA' alt='picture'/>
                             <div className='featureText'>
                                 <h3 className='featureTitle'>Track Balances</h3>
                                 <p className='featureCaption'> Keep track of shared expenses, balances, and who owes who.</p>
                             </div>
                         </div>
                         <div className='features'>
-                            <img className='featureLogo' src='https://spshurl.herokuapp.com/C0Aa4' alt='picture'/>
+                            <img className='featureLogo' src='https://s3-alpha-sig.figma.com/img/f2bc/943b/ba22f83189c8b55b18a522512bfd1996?Expires=1620604800&Signature=hodhqLM8TJsLOvzNNNXAKniNIjCCk5LmKzGmBW9ewZFPW5D2fyzoFRRzviPNeewpEFOErI-2iqBThEFKH0p99XAp9ZR3p3SBmf9R5rafX73qI3-2vrfH60qFBw1aGubLhi5itsOVq-f4yyeJkewSynFWkqrPC7prZyHlVGbQnQdSJHUl7CdhA7n3nshbA2eNzlEgVZdUPPne-NgvBAhQv30GHAuSmBRrcDiO1~vbWelXE3F1T7gHUjnF2GwjGPR1MR86iM49BFw1t4Psg8AsWobh~6GRam24Z95e6FWfXANsU6BFN7E2Chw2zzmitCJof7uTQOOSwCZQnpdwMjn0Ig__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA' alt='picture'/>
                             <div className='featureText'>
                                 <h3 className='featureTitle'>Organize Expenses</h3>
                                 <p className='featureCaption'> Split expenses with any group: trips, housemates, friends, and family.</p>
                             </div>
                         </div>
                         <div className='features'>
-                            <img className='featureLogo' src='https://spshurl.herokuapp.com/C0Aa4' alt='picture'/>
+                            <img className='featureLogo' src='https://s3-alpha-sig.figma.com/img/1d5a/a7f8/2864d9306d6d1d8c2dd1645acdc56b9a?Expires=1620604800&Signature=Mzyf~XatrjDf7WlJwgT-2ipC6dsyrjYLSPqmkNnlclhjC-8sWkZH4ft255dNAXWLLCx5bhGiwTeA9suHH6FAXIP-BLCi6y1CbD2fPyT28jK0es9HUcE6BykrL~k03Yt233~FweEz0cLEzvMmoiFX2BuI5VbWTnNBCLvInW5a0xVn~DX4Fw7xKmC~-~l2Ch-Z0F~pgKidqaWKQlHIV5jvHkIhCvouyR0mWIkQkpjwJDwsSBsZ8IfzRrySojeAsThJpjAsgYJmaoAWZqnS6W6ibWQp6fX269yajFBOMWuWZmuM9rHCbeb6M-D9C6R1GK0BLLDEzjYdNNhbJnCWKpVKeg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA' alt='picture'/>
                             <div className='featureText'>
                                 <h3 className='featureTitle'>Add Expenses Easily</h3>
                                 <p className='featureCaption'> Quickly add expenses on the go before you forget who paid.</p>
                             </div>
                         </div>
                         <div className='features'>
-                            <img className='featureLogo' src='https://spshurl.herokuapp.com/C0Aa4' alt='picture'/>
+                            <img className='featureLogo' src='https://s3-alpha-sig.figma.com/img/7191/83c0/fd6f68de9adfe9036dde8ce3998c21d9?Expires=1620604800&Signature=KTc-YLyF7TCKA8ooSByKOQS-vnE0Ev8aEx6y3hYBCZkW0O4hBCILgqki-44bNczweObMIaUZP5fBBSLdavlW3ZaMAI7AFelCqNjHBYzLVmF6FJ9jaPtD6xKNwFQuHOeC7feQUIHvIQQMAM9~BI05ngT5BYR1aBGtZaG3JbIJGQKAl4OfmTNcv3lkfspm80dmMk007Gwc~PsjJ6OBjFuf~q-9Ju~rh38aY3DPOLyvT6-5OLh0wGrt3rdps7yqs1VOzaU6C79I~B21LzgLVDurje6TXsQSgNayUqDHjowN3Umklknm10wiDcI01fesa5Ns0qWLLP~s~jkrgITK2NtIWw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA' alt='picture'/>
                             <div className='featureText'>
                                 <h3 className='featureTitle'>Pay Friends Back</h3>
                                 <p className='featureCaption'> Settle up with friend and record any cash or online payment.</p>
